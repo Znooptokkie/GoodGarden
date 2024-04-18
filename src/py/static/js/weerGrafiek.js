1523a276f37bbe1e7bce627e378e8f29ad348331
Atilla <oomenatilla@gmail.com>
2024-04-18 13:18:12 +0200

    Grafiek en Warning Image werkend

--- src/py/static/js/weerGrafiek.js
+++ src/py/static/js/weerGrafiek.js
@@ -40,8 +40,8 @@ function fetchWeatherDataAndDrawChart(canvasId, apiUrl) {
                     labels: dates,
                     datasets: [{
                         data: temperatures,
-                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                        borderColor: 'rgba(54, 162, 235, 1)',
+                        backgroundColor: 'rgba(143, 188, 143, 0.2)',
+                        borderColor: 'rgba(143, 188, 143, 1)',
                         borderWidth: 1
                     }]
                 },
