0b9caf2042a833449b389913cc9b2294dcb8f2ff
Znooptokkie <oomenatilla@gmail.com>
2024-05-15 12:19:54 +0200

    Planten API basis gelegd

---
+++ src/py/static/js/plantenAPI.js
@@ -0,0 +1,18 @@
+function fetchPlantenAPI (apiUrl)
+{
+    fetch(apiUrl).then(response => response.json()).then(data => 
+    {
+
+        console.log(data.data);
+
+        for (const plant of data.data) 
+        {
+            console.log(plant);    
+        }
+    });
+}
+ 
+document.addEventListener("DOMContentLoaded", () =>
+{
+    fetchPlantenAPI("https://perenual.com/api/species-list?key=sk-CZEB66431774a19e35471");
+});
