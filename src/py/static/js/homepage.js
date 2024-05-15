7767c0eabd21687eded6c937fdb63c40b96e8f56
6028570 <mohammedcifci78@gmail.com>
2024-05-15 13:53:12 +0200

    plant modal checker werkt

--- src/py/static/js/homepage.js
+++ src/py/static/js/homepage.js
@@ -27,17 +27,63 @@ function openModal(side)
     {
         modal.style.display = "none";
     };
-
-    submit.onclick = function () 
-    {
-        // alert("Plant toegevoegd");
-        modal.style.display = "none";
+    submit.onclick = function () {
+        const plantNaamInput = document.getElementById("plantNaam");
+        const plantNaam = plantNaamInput.value.trim(); 
+    
+        const plantensoortSelect = document.getElementById("plantensoort");
+        const plantensoort = plantensoortSelect.value;
+    
+        if (plantNaam === "" || plantensoort === "") {
+            const emptyModal = document.createElement("article");
+            emptyModal.classList.add("modal", "modal-content");
+            emptyModal.style.display = "block"; 
+    
+            const modalContent = document.createElement("article");
+            modalContent.classList.add("modal-items");
+            const errorMessage = document.createElement("h2");
+            errorMessage.textContent = "Je hebt niet alle velden ingevuld.";
+            modalContent.appendChild(errorMessage);
+    
+            const okButton = document.createElement("button");
+            okButton.classList.add("okButton");
+            okButton.textContent = "Ok";
+            okButton.onclick = function () {
+                emptyModal.style.display = "none"; 
+            };
+            modalContent.appendChild(okButton);
+    
+            emptyModal.appendChild(modalContent);
+    
+            document.body.appendChild(emptyModal);
+        } else {
+            const okModal = document.createElement("article");
+            okModal.classList.add("modal", "modal-content");
+            okModal.style.display = "block"; 
+        
+            const modalContent = document.createElement("article");
+            modalContent.classList.add("modal-items");
+            const successMessage = document.createElement("h2");
+            successMessage.textContent = "Plant is succesvol toegevoegd";
+            modalContent.appendChild(successMessage);
+        
+            const okButton = document.createElement("button");
+            okButton.classList.add("okButton");
+            okButton.textContent = "Ok";
+            okButton.onclick = function () {
+                okModal.style.display = "none"; 
+                modal.style.display = "none"; 
+            };
+            modalContent.appendChild(okButton);
+        
+            okModal.appendChild(modalContent);
+        
+            document.body.appendChild(okModal);
+        }
     };
-
-    window.onclick = function (event) 
-    {
-        if (event.target === modal) 
-        {
+    
+    window.onclick = function (event) {
+        if (event.target === modal) {
             modal.style.display = "none";
         }
     };
